0adc2024d79703237ab5d076c70c694b456ec319
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-02-21 15:56:59 -0300

    fix: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './User/user.module';
 import { PrismaModule } from './api/prisma.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {  }
