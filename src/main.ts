99c4bfd3772bbb659905a4b026463c6e95e6ea56
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-02-21 16:54:26 -0300

    fix: Port

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
     },
   });
 
-  await app.listen(3000);
+  const Port = process.env.PORT || 3000;
+
+  await app.listen(Port).then(() => {
+    console.log(`Server started on port ${Port}`);
+  });
 }
 bootstrap();
+
