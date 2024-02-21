81abad6ec75b41cba1b076f1757b53a06004ca05
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-02-21 15:53:05 -0300

    fix: auth module and service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { UserService } from 'src/User/user.service';
-import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private userService: UserService,
-    private jwtService: JwtService,
-  ) {}
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.userService.findOne(email);
-    console.log('user', user);
+  constructor(private jwtService: JwtService) { }
 
-    if (user) {
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (isMatch) {
-        // const { password, ...result } = user;
-        return user;
-      }
-    }
-    return null;
-  }
   async login(user: any) {
-    const payload = { email: user.email, sub: user.userId };
+    const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
