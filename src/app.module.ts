5a96a65d797440a48d5aba7fff4ebfe545106c7b
Ana Beatriz Barreto <abbmr@hotmail.com>
2024-02-20 21:25:40 -0300

    fix/ falta do res sendo passado no use do middle ocasionava do next não ser lido como função, consertado.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,12 @@
-import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserModule } from './User/user.module';
 import { PrismaModule } from './api/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ValidateIdMiddleware } from './validate-id/validate-id.middleware';
-import { UserController } from './User/user.controller';
 
 @Module({
   imports: [PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(ValidateIdMiddleware)
-      .exclude({path: 'users', method: RequestMethod.POST})
-      .forRoutes(UserController);
-  }
-}
+export class AppModule {}
--- src/validate-id/validate-id.middleware.ts
+++ src/validate-id/validate-id.middleware.ts
@@ -1,18 +1,17 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction, Request} from 'express';
+import { NextFunction, Request } from 'express';
 import { z } from 'zod';
 
 @Injectable()
 export class ValidateIdMiddleware implements NestMiddleware {
-  use (req: Request, next: NextFunction) {
-
+  use (req: Request, res: Response, next: NextFunction) {
     const HexString12BytesSchema = z.string().refine(value => /^[0-9a-fA-F]{24}$/.test(value));
-
     const result = HexString12BytesSchema.safeParse(req.params.id);
 
     if (!result.success) {
       throw new Error('Invalid ID');
     }
+
     next();
   }
 }
